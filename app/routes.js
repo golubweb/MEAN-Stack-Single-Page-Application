0467ee3a0cba2b822fd4c378bc33df1f86873bc9
darko <darko.golubovic@hotmail.rs>
2016-10-18 17:20:15 +0200

    login fix

--- app/routes.js
+++ app/routes.js
@@ -28,23 +28,27 @@ router.get('/login', function(req, res){
 	res.render('login.ejs');
 });
 
-app.post('/login-user', function(req, res){
-
+app.post('/login-user', function(req, res) {
 	if(req.method == 'POST') {
 		var email = req.body.user_email;
 		var pass = req.body.user_pass;
 
 		if(email !== false && pass !== false) {
 			db.query('SELECT admin_id, admin_name, admin_email, admin_hash FROM admin WHERE admin_email="'+ email +'" AND admin_password="'+ pass +'" AND admin_stats="1"', function(error, rows, fields){
-				var admin_data = rows;
-				res.render('admin.ejs', {info: admin_data});
-				res.end();
+				var data = rows[0];
+
+				if(rows[0].admin_hash !== undefined){
+					res.render('admin.ejs', {info: data});
+					res.end();
+				} else {
+					res.redirect('/login');
+					res.end();
+				}
 			});
-		} else {
-			res.redirect('/login');
 		}
 	} else {
 		res.redirect('/login');
+		res.end();
 	}
 });
 
@@ -60,7 +64,6 @@ router.get('/post/:id', function(req, res) {
 	//var post_url = path.join(__dirname, 'templates');
 
 	db.query('SELECT * FROM post WHERE post_id="' + post_id + '"' , function(error, rows, fields) {
-
 		if(rows[0].post_stats == 1) {
 			res.render('post.ejs', {item: rows[0]});
 		} else {
