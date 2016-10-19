1afc92dcfe22b5b01ee7aafba7402a4a73d2e872
darko <darko.golubovic@hotmail.rs>
2016-10-19 17:07:44 +0200

    logout page

--- app/routes.js
+++ app/routes.js
@@ -10,6 +10,8 @@ var express = require('express');
 var app     = express();
 var port    = process.env.PORT || 8888;
 var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+//var expressValidator = require('express-validation');
 
 // ROUTES
 // ==============================================
@@ -21,11 +23,27 @@ var router = express.Router();
 // Support encoded bodies
 app.use(bodyParser.urlencoded({ extended: true }));
 
+//Support Cookie
+app.use(cookieParser());
+
+//app.use(expressValidator());
+
 // Support static folder for script and style
 app.use(express.static(path.join(__dirname, 'public')));
+app.locals.loginMsg = 'Please enter Email and Password';
 
 router.get('/login', function(req, res){
 	res.render('login.ejs');
+});
+
+router.post('/logout', function(req, res){
+	if(req.method == 'POST') {
+		res.clearCookie('userHash');
+		res.redirect('/login');
+		res.end();
+	} else {
+		res.end();
+	}
 });
 
 app.post('/login-user', function(req, res) {
@@ -33,21 +51,30 @@ app.post('/login-user', function(req, res) {
 		var email = req.body.user_email;
 		var pass = req.body.user_pass;
 
+		//req.checkBody('email', 'Name is required').notEmpty();
+
+		//var error = req.validationErrors();
+
 		if(email !== false && pass !== false) {
 			db.query('SELECT admin_id, admin_name, admin_email, admin_hash FROM admin WHERE admin_email="'+ email +'" AND admin_password="'+ pass +'" AND admin_stats="1"', function(error, rows, fields){
 				var data = rows[0];
 
-				if(rows[0].admin_hash !== undefined){
+				if(Object.keys(rows).length > 0){
+					console.log(data.admin_hash);
+					res.cookie('userHash', data.admin_hash, {maxAge: 1000});
+					//res.clearCookie('userHash');
+
 					res.render('admin.ejs', {info: data});
 					res.end();
 				} else {
+					app.locals.loginMsg = 'Email or Password is incorrect!';
 					res.redirect('/login');
 					res.end();
 				}
 			});
 		}
 	} else {
-		res.redirect('/login');
+		console.log('Is not Post Method');
 		res.end();
 	}
 });
@@ -78,5 +105,6 @@ router.get('/post/:id', function(req, res) {
 
 // Run Router
 app.use('/', router);
+
 // Run Server
 http.createServer(app).listen(port);
